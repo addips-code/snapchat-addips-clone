39d866d5f32d5290a8ab3b2c9571c2498ec7eae7
adeola adeniji <addips002@gmail.com>
2022-12-05 17:59:42 +0100

    commit

--- src/ChatView.js
+++ src/ChatView.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import './ChatView.css';
 import { selectSelectedImage } from './features/appSlice';
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 function ChatView() {
     const selectedImage = useSelector(selectSelectedImage);
@@ -15,13 +16,33 @@ function ChatView() {
     }, [selectedImage])
 
     const exit = () => {
-        navigate('/chats')
-    }
+        navigate('/chats');
+    };
+    
   return (
     <div className='chatview'>
         <img src={selectedImage} onClick={exit} alt="" />
+        <CountdownCircleTimer
+            isPlaying
+            duration={10}
+            strokeWidth={6}
+            size={50}
+            colors={[
+                ["#004777", 0.33],
+                ["#F7B801", 0.33],
+                ["#A30000", 0.33],
+            ]}
+        >
+            {({remainingTime}) =>{
+                if (remainingTime === 0 ){
+                    exit();
+                }
+
+                return remainingTime;
+            }}
+        </CountdownCircleTimer>
     </div>
   )
 }
 
-export default ChatView
+export default ChatView;
